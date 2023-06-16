42691c05c629507985f5293b5ef611db5fffe138
zainebbenrabha <zinebbenrrabha@gmail.com>
2023-06-16 16:09:45 -0700

    task

--- 0x01-variables_if_else_while/2-print_alphabet.c
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -1,32 +1,19 @@
-#include <stdlib.h>
-#include <time.h>
 #include <stdio.h>
 /**
- * main - Prints the last digit of a randomly generated number
- * and whether it is greater than 5, less than 6, or 0.
+ * main - Prints the alphabet.
  *
- * Return: Always 0.
+ * Return: Always 0 (Success)
  */
 int main(void)
 {
-	int n;
-	srand(time(0));
-	n = rand() - RAND_MAX / 2;
+	char alp[26] = "abcdefghijklmnopqrstuvwxyz";
+	int i;
 
-	if ((n % 10) > 5)
+	for (i = 0; i < 26; i++)
 	{
-		printf("Last digit of %d is %d and is greater than 5\n",
-			n, n % 10);
+		putchar(alp[i]);
 	}
-	else if ((n % 10) < 6 && (n % 10) != 0)
-	{
-		printf("Last digit of %d is %d and is less than 6 and not 0\n",
-			n, n % 10);
-	}
-	else
-	{
-		printf("Last digit of %d is %d and is 0\n",
-		n, n % 10);
-	}
+	putchar('\n');
 	return (0);
 }
+
