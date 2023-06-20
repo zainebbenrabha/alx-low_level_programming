1e97880a6702eb30b2bfc7566f436728840308f3
zainebbenrabha <zinebbenrrabha@gmail.com>
2023-06-20 16:22:20 -0700

    0-putchar.c

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -1,21 +1,21 @@
-1 #include "main.h"
-2 /**
-3 *main - Entry point
-4 *description: 'prints _putchar to stdout'
-5 *Return: always 0
-6 */
-7 int main(void)
-8 {
-9	_putchar('H');
-10	_putchar('o');
-11	_putchar('l');
-12	_putchar('b');
-13	_putchar('e');
-14      _putchar('r');
-15	_putchar('t');
-16	_putchar('o');
-17	_putchar('n');
-18	_putchar('\n');
-19	return (0);
-20}
+#include "main.h"
+
+/**
+ * main -  prints _putchar, followed by a new line
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	_putchar('_');
+	_putchar('p');
+	_putchar('u');
+	_putchar('t');
+	_putchar('c');
+	_putchar('h');
+	_putchar('a');
+	_putchar('r');
+	_putchar('\n');
+
+	return (0);
+}
 
