eb402aad29efd867e57f9c18dd2267ae618a98fd
zainebbenrabha <zinebbenrrabha@gmail.com>
2023-06-22 17:10:56 -0700

    6-print_line.c

--- 0x04-more_functions_nested_loops/6-print_line.c
+++ 0x04-more_functions_nested_loops/6-print_line.c
@@ -21,22 +21,3 @@ void print_line(int n)
 	}
 
 }
-
-
-6-main.c
-#include "main.h"
-
-/**
- * main - check the code
- *
- * Return: Always 0.
- */
-int main(void)
-{
-    print_line(0);
-    print_line(2);
-    print_line(10);
-    print_line(-4);
-    return (0);
-}
-
