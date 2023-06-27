1eeb1699efccf42ef2881541b8ef3a7912b8a7a3
zainebbenrabha <zinebbenrrabha@gmail.com>
2023-06-26 19:22:39 -0700

    2-strlen.c

--- 0x05-pointers_arrays_strings/2-strlen.c
+++ 0x05-pointers_arrays_strings/2-strlen.c
@@ -4,14 +4,16 @@
  * @s: string
  * Return: length
  */
-int_strlen(char *s)
+int _strlen(char *s)
 {
-	int longi=0;
+	int longi = 0;
 
 	while (*s != '\0')
 	{
 		longi++;
 		s++;
 	}
+
 	return (longi);
 }
+
