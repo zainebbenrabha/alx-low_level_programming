04ba1500596943727892d68088441690178c7d52
zainebbenrabha <zinebbenrrabha@gmail.com>
2023-07-05 14:00:01 -0700

    task0

--- 0x08-recursion/0-puts_recursion.c
+++ 0x08-recursion/0-puts_recursion.c
@@ -2,17 +2,17 @@
 /**
  * _puts_recursion - function like puts();
  * @s: input
- * Return: Always 0 (success)
+ * Return: Always 0 (Success)
  */
-void _put_recursion(char*s)
+void _puts_recursion(char *s)
 {
 	if (*s)
 	{
 		_putchar(*s);
 		_puts_recursion(s + 1);
-
 	}
-	
+
 	else
 		_putchar('\n');
 }
+
