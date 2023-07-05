10b4cd868a27b5c77c6bdc8fe8154a9b245277e4
zainebbenrabha <zinebbenrrabha@gmail.com>
2023-07-05 15:47:04 -0700

    task5

--- 0x08-recursion/5-sqrt_recursion.c
+++ 0x08-recursion/5-sqrt_recursion.c
@@ -1,6 +1,8 @@
 #include "main.h"
+int actual_sqrt_recursion(int n, int i);
+
 /**
- * _sprt_recursion - returns the natural squqre root of a number
+ *_sprt_recursion - returns the natural squqre root of a number
  * @n: number to calculate the squqre root of
  *
  * Return: the resulting square root
@@ -9,5 +11,5 @@ int _sqrt_recursion(int n)
 {
 	if (n < 0)
 		return (-1);
-	return (actual_aqrt_recursion(n,0));
+	return (actual_sqrt_recursion(n,0));
 }
