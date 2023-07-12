6fd0fdaf14e177881e7edab95d23042cb3c1da63
zainebbenrabha <zinebbenrrabha@gmail.com>
2023-07-12 16:11:41 -0700

    task0

--- 0x0A-argc_argv/main.h
+++ 0x0A-argc_argv/main.h
@@ -1,9 +1,15 @@
+#ifndef MAIN_H
+		
 #define MAIN_H
-                
-                
+		
+
+		
 int _putchar(char c);
-                
+		
 int _atoi(char *s);
-                          
-# endif
+		
+
+		
+#endif
+
 
